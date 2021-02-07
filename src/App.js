41d6992b09ac3f7a4fd479f7bc924c3f7232ad78
josemvcerqueira <jose@ilovemochi.com>
2021-02-07 02:04:28 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useEffect, useState, useRef } from 'react'
 import './App.css';
+import io from "socket.io-client";
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 
 function App() {
+  const [accessToken, setAccessToken] = useState(null)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const socket = useRef(null)
+  useEffect(() => {
+    // How to handle authentication
+    // On APP Error should refresh the accessToken and retry once again
+      socket.current =  io('http://localhost:5000/driver', {
+        auth: {
+          token: accessToken
+        },
+      })
+
+    return () => {
+        // Disconnect if it unmounts
+        if(socket.current) socket.current.disconnect()
+    }
+  }, [])
+
+  const handleLogin = async () => {
+    const result = await fetch('http://localhost:5000/api/v1/login/user', {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include',
+    })
+
+    const data = await result.json()
+
+    setAccessToken(data.accessToken)
+  }
+
+  const handleRandomCoordinates = () => {
+    if (socket.current) {
+      socket.current.emit('track-order', {driverLocationId: 1, coordinates: [getRandomInt(10), getRandomInt(12)] })
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="email" value={email} placeholder="your email" onChange={event => setEmail(event.target.value)}/>
+      <input type="password" value={password} placeholder="your password" onChange={event => setPassword(event.target.value)}/>
+      <button type="button" onClick={handleLogin}>login</button>
+      <button onClick={handleRandomCoordinates}>send random coordinates</button>
     </div>
   );
 }
